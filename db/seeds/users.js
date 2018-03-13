9eedb1958a51addde73ec698436486738f730150
David Fok <david_fok@hotmail.com>
2018-03-12 18:49:02 -0700

    added user to DB seed

---
+++ db/seeds/users.js
@@ -0,0 +1,19 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {
+          first_name: 'bla',
+          last_name: 'bla',
+          student_id: 12345,
+          password_digest: 12345,
+          email: 'bla@bla.bla',
+          handicap: false,
+          address_id: 7
+        }
+      ]);
+    });
+};
